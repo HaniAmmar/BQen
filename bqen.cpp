a6c81385feac35d3424f12de79ee1df1928c3a12
Hani Ammar <hani.code@outlook.com>
2020-11-01 04:53:32 +0300

    Improve performance

--- bqen.cpp
+++ bqen.cpp
@@ -19,8 +19,10 @@ ZEND_GET_MODULE(bqen)
 #endif
 
 PHP_FUNCTION(BQen_Render) {
-    using QValue = Qentem::Value<char>;
+    using QTagBit = Qentem::Array<Qentem::Template::TagBit<char>>;
+
     static Qentem::StringStream<char> template_ss;
+    static QTagBit                    tags_cache;
 
     size_t template_str_len = 0;
     char * template_str     = nullptr;
@@ -32,22 +34,21 @@ PHP_FUNCTION(BQen_Render) {
     ZEND_PARSE_PARAMETERS_END();
 
     template_ss.Clear();
+    tags_cache.Clear();
 
     if ((template_str_len != 0) && (template_str != nullptr)) {
         if (Z_TYPE_P(value) == IS_ARRAY) {
             Qentem::Template::Render(template_str, template_str_len,
                                      static_cast<const BQen::BQ_ZVAL *>(value),
-                                     &template_ss);
+                                     &template_ss, &tags_cache);
         } else if (Z_TYPE_P(value) == IS_STRING) {
-            const QValue qval =
+            const Qentem::Value<char> qval =
                 Qentem::JSON::Parse(Z_STRVAL_P(value), Z_STRLEN_P(value));
 
             Qentem::Template::Render(template_str, template_str_len, &qval,
-                                     &template_ss);
+                                     &template_ss, &tags_cache);
         }
-
-        RETURN_STRINGL(template_ss.First(), template_ss.Length());
     }
 
-    RETURN_BOOL(false);
+    RETURN_STRINGL(template_ss.First(), template_ss.Length());
 }
--- bqen_arginfo.h
+++ bqen_arginfo.h
@@ -1,6 +1,6 @@
 /* This is a generated file, edit the .stub.php file instead. */
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_BQen_Render, 0, 2, MAY_BE_STRING | MAY_BE_FALSE)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(arginfo_BQen_Render, 0, 2, MAY_BE_STRING)
 ZEND_ARG_TYPE_INFO(0, tempale, IS_STRING, 0)
 ZEND_ARG_INFO(0, value)
 ZEND_END_ARG_INFO()
